3d90ae100ff38fbeeddb7fdae7a95c2019aff6cc
vlademy <85895659+vlademy@users.noreply.github.com>
2021-07-14 11:36:51 -0700

    Adding https://github.com/celery/celery/issues/4185#issuecomment-373085496 fix for possible hanging workers

--- celery/concurrency/asynpool.py
+++ celery/concurrency/asynpool.py
@@ -713,6 +713,7 @@ class AsynPool(_pool.Pool):
             except KeyError:
                 pass
         self.on_inqueue_close = on_inqueue_close
+        self.hub_remove = hub_remove
 
         def schedule_writes(ready_fds, curindex=[0]):
             # Schedule write operation to ready file descriptor.
@@ -1217,6 +1218,7 @@ class AsynPool(_pool.Pool):
             if queue:
                 for sock in (queue._reader, queue._writer):
                     if not sock.closed:
+                        self.hub_remove(sock)
                         try:
                             sock.close()
                         except (IOError, OSError):
