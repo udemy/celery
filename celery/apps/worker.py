7661e6e98e7114b643672bb7451c2ec42d017fd8
John Fesenko <jfesenko@gmail.com>
2025-02-03 17:03:36 -0800

    add debug prints for _shutdown_handler

--- celery/apps/worker.py
+++ celery/apps/worker.py
@@ -13,7 +13,7 @@ import sys
 from datetime import datetime
 from functools import partial
 
-from billiard.common import REMAP_SIGTERM
+from billiard.common import REMAP_SIGTERM, TERMSIGS_IGNORE, TERMSIGS_FORCE
 from billiard.process import current_process
 from kombu.utils.encoding import safe_str
 
@@ -280,13 +280,16 @@ class Worker(WorkController):
 
 def _shutdown_handler(worker, sig='TERM', how='Warm',
                       callback=None, exitcode=EX_OK):
+    safe_say(f"_shutdown_handler(); worker={worker}; sig={sig};how={how}", sys.__stdout__)
     def _handle_request(*args):
+        safe_say(f"inside _shutdown_handler._handle_request; args={args}; REMAP_SIGTERM={REMAP_SIGTERM}; TERMSIGS_IGNORE={TERMSIGS_IGNORE}; TERMSIGS_FORCE={TERMSIGS_FORCE}", sys.__stdout__)
         with in_sighandler():
             from celery.worker import state
             if current_process()._name == 'MainProcess':
                 if callback:
                     callback(worker)
-                safe_say(f'worker: {how} shutdown (MainProcess)', sys.__stdout__)
+                safe_say(f'worker: {how} shutdown (MainProcess#1)', sys.__stdout__)
+                safe_say(f"_shutdown_handler._handle_request; worker.hostname={worker.hostname}; sig={sig}, how={how}, exitcode={exitcode}", sys.__stdout__)
                 signals.worker_shutting_down.send(
                     sender=worker.hostname, sig=sig, how=how,
                     exitcode=exitcode,
